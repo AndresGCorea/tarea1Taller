e0fd065d8fdb8489cf93f103e45683b9257caa9c
AndresGCorea <125699110+AndresGCorea@users.noreply.github.com>
2023-03-16 13:58:11 -0600

    Add files via upload

---
+++ tarea01.py
@@ -0,0 +1,43 @@
+print(""" alt201=╔ alt186=║ alt187=╗ alt188=╝ alt200╚ alt= ═
+╔════════════════════════════════════════════════════════════════════════════════════════════════╗
+║    Bienvenido a la bitacora                                                                    
+║    Por favor digite un número para realizar una operacion del menú de bitacora        
+║    Nombre del menu                    | Funcion                                       
+║    1= Registro actividad                                            
+║    2=Búsquedas
+║    3=Cantidad de mensajes
+║    4=Guardar mensaje
+║    9=Salir
+║    11=Resgistrar actividad
+║    12=Borrar actividad
+║    13=Modificar actividad
+║    14=Ver todos los mensajes
+║    15=Retornar
+║    21=Buscar por usuario
+║    22=Buscar por aplicación
+║    23=Buscar por fecha
+║    24=Retornar                                                                                ←
+║                                                                          Version 1.0  ←
+╚════════════════════════════════════════════════════════════════════════════════════════════════╝
+""")
+
+def bitacora (menu):
+    if isinstance(menu,int)>0:
+        input("Por favor digite su nombre")
+        input("digite la hora")
+        input("digite ")
+
+
+def
+def añadirActividad(menu):
+    if menu==#numero de añadirActividad:
+
+
+
+def modificarActividad(menu):
+
+def guardarActividad(menu):
+
+def BorrarActividad(menu):
+
+
